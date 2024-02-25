ac6184350aa98c1c8b533c15eb377f88415b3acb
Mizhik <vlad.smilono2018@gmail.com>
2024-02-25 23:29:17 +0200

    add inlinebutton for product (functional delete and change)

--- heandlers/admin_private.py
+++ heandlers/admin_private.py
@@ -1,12 +1,13 @@
 from aiogram import F, Router, types
-from aiogram.filters import Command, StateFilter
+from aiogram.filters import Command, StateFilter,or_f
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from database.orm_query import orm_add_product, orm_get_products
+from database.orm_query import orm_add_product, orm_delete_product, orm_get_product, orm_get_products, orm_update_product
 
 from filters.chat_types import ChatTypeFilter, IsAdmin
+from kbds.inline import get_callback_btns
 from kbds.reply import get_keyboard
 
 
@@ -21,7 +22,20 @@ ADMIN_KB = get_keyboard(
     placeholder="Виберіть дію",
     sizes=(2,),
 )
+class AddProduct(StatesGroup):
+    name = State()
+    description = State()
+    price = State()
+    image = State()
 
+    product_for_change = None
+
+    texts = {
+        'AddProduct:name': 'Введіть назву заново:',
+        'AddProduct:description': 'Введіть опис заново:',
+        'AddProduct:price': 'Введіть ціну заново:',
+        'AddProduct:image': 'Цей state останній...',
+    }
 
 @admin_router.message(Command("admin"))
 async def add_product(message: types.Message):
@@ -34,27 +48,39 @@ async def starring_at_product(message: types.Message, session: AsyncSession):
         await message.answer_photo(
             product.image,
             caption=f'<strong>{product.name}\
-                    <strong>\n{product.description}\nЦіна: {round(product.price,2)}',
+                    </strong>\n{product.description}\nЦіна: {round(product.price,2)}',
+            reply_markup=get_callback_btns(btns={
+                "Видалити": f"delete_{product.id}",
+                "Змінити":f"change_{product.id}",
+                }
+            ),
         )
     await message.answer("ОК, ось список товарі")
 
+@admin_router.callback_query(F.data.startswith('delete_'))
+async def delete_product(callback: types.CallbackQuery, session:AsyncSession):
+    
+    product_id = callback.data.split("_")[-1]
+    await orm_delete_product(session, int(product_id))
+
+    await callback.answer("Товар видалено")
+    await callback.message.answer("Товар видалено!")
+
+@admin_router.callback_query(StateFilter(None), F.data.startswith("change_"))
+async def change_product_callback(callback: types.CallbackQuery, state: FSMContext, session: AsyncSession):
+    product_id = callback.data.split("_")[-1]
+
+    product_for_change = await orm_get_product(session, int(product_id))
+
+    AddProduct.product_for_change = product_for_change
+
+    await callback.answer()
+    await callback.message.answer(
+        "Введіть назву товару", reply_markup=types.ReplyKeyboardRemove())
+    await state.set_state(AddProduct.name)
 
 
 #Код ниже для машины состояний (FSM)
-
-class AddProduct(StatesGroup):
-    name = State()
-    description = State()
-    price = State()
-    image = State()
-
-    texts = {
-        'AddProduct:name': 'Введіть назву заново:',
-        'AddProduct:description': 'Введіть опис заново:',
-        'AddProduct:price': 'Введіть ціну заново:',
-        'AddProduct:image': 'Цей state останній...',
-    }
-
 @admin_router.message(StateFilter(None),F.text == "Додати товар")
 async def add_product(message: types.Message, state: FSMContext):
     await message.answer(
@@ -69,7 +95,8 @@ async def cancel_handler(message: types.Message, state: FSMContext) -> None:
     current_state = await state.get_state()
     if current_state is None:
         return
-
+    if AddProduct.product_for_change:
+        AddProduct.product_for_change = None
     await state.clear()
     await message.answer("Дія відмінена", reply_markup=ADMIN_KB)
 
@@ -91,40 +118,73 @@ async def cancel_handler(message: types.Message, state: FSMContext) -> None:
             return
         previous = step
 
-@admin_router.message(AddProduct.name,F.text)
+@admin_router.message(AddProduct.name,or_f(F.text, F.text == '.'))
 async def add_name(message: types.Message, state: FSMContext):
-    await state.update_data(name=message.text)
+    if message.text == ".":
+        await state.update_data(name=AddProduct.product_for_change.name)
+    else:
+        await state.update_data(name=message.text)
     await message.answer("Введіть опис товару")
     await state.set_state(AddProduct.description)
 
 
 @admin_router.message(AddProduct.name)
-async def add_name(message: types.Message, state: FSMContext):
+async def add_name2(message: types.Message, state: FSMContext):
     await message.answer("Ви ввели недопустимі данні, введіть назву товару")
 
 
-@admin_router.message(AddProduct.description,F.text)
+@admin_router.message(AddProduct.description,or_f(F.text, F.text == '.'))
 async def add_description(message: types.Message, state: FSMContext):
-    await state.update_data(description = message.text)
+    if message.text == '.':
+        await state.update_data(description = AddProduct.product_for_change.description)
+    else:
+        await state.update_data(description = message.text)
     await message.answer("Введіть ціну товару")
     await state.set_state(AddProduct.price)
 
-@admin_router.message(AddProduct.price, F.text)
+@admin_router.message(AddProduct.description)
+async def add_description2(message: types.Message, state: FSMContext):
+    await message.answer("Ви ввели недопустимі данні, введіть опис товару")
+
+
+@admin_router.message(AddProduct.price, or_f(F.text, F.text == '.'))
 async def add_price(message: types.Message, state: FSMContext):
+    if message.text == '.':
+        await state.update_data(price = AddProduct.product_for_change.price)
+    try:
+        float(message.text)
+    except ValueError:
+        await message.answer("Введіть коректне значення ціни")
+
     await state.update_data(price = message.text)
     await message.answer("Завантажити зображення товару")
     await state.set_state(AddProduct.image)
 
-@admin_router.message(AddProduct.image,F.photo)
+@admin_router.message(AddProduct.price)
+async def add_price2(message: types.Message, state: FSMContext):
+    await message.answer("Ви ввели недопустимі данні, введіть ціну товару")
+
+
+@admin_router.message(AddProduct.image,or_f(F.photo, F.text == '.'))
 async def add_image(message: types.Message, state: FSMContext, session: AsyncSession):
     
-    await state.update_data(image = message.photo[-1].file_id)
+    if message.text and message.text == '.':
+        await state.update_data(image = AddProduct.product_for_change.image)
+    else:
+        await state.update_data(image = message.photo[-1].file_id) 
     data = await state.get_data()
     try:
-        await orm_add_product(session,data)
-        await message.answer("Товар додано", reply_markup=ADMIN_KB)
+        if AddProduct.product_for_change.image:
+            await orm_update_product(session, AddProduct.product_for_change.id,data)
+        else:
+            await orm_add_product(session,data)
+        await message.answer("Товар додано/змінено", reply_markup=ADMIN_KB)
         await state.clear()
     except Exception as e:
         await message.answer(
         f"Помилка: \n{str(e)}\n", reply_markup=ADMIN_KB)
         await state.clear()
+    AddProduct.product_for_change = None
+@admin_router.message(AddProduct.image)
+async def add_image2(message: types.Message, state: FSMContext):
+    await message.answer("Відправте фото товару")
--- new_bot.py
+++ new_bot.py
@@ -20,7 +20,7 @@ from heandlers.admin_private import admin_router
 
 from common.bot_cmds_list import private
 
-ALLOWED_UPDATES = ['message,edited_message']
+#ALLOWED_UPDATES = ['message','edited_message']
 
 bot = Bot(token=os.getenv('TOKEN'), parse_mode= ParseMode.HTML)
 bot.my_admins_list = []
@@ -49,6 +49,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     #await bot.delet_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
-    await dp.start_polling(bot,allowed_updates= ALLOWED_UPDATES)
+    await dp.start_polling(bot,allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
